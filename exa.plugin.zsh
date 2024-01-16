cbbec2e428c7bbba11e95cebc0da49a71e4fe8db
Marcin Gryszko <mgryszko@gmail.com>
2024-01-16 07:51:22 +0100

    Use eza instead of exa

--- exa.plugin.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-alias ls='exa'
-alias ll='exa -l'
-alias la='exa -l'
-alias l='exa -la'
-alias lsa='exa -la'
---
+++ eza.plugin.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+alias ls='eza'
+alias ll='eza -l'
+alias la='eza -l'
+alias l='eza -la'
+alias lsa='eza -la'
